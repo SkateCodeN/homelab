d881df4e07a4aaa961b0e3c14ad1239a0153cd0f
Grey <immemo1105@gmail.com>
2024-01-20 14:28:24 -0800

    Logs has nav and is sticky

--- logs.php
+++ logs.php
@@ -1,24 +1,51 @@
-<html>
-    <head>
-        <title>Add Log</title>
-    </head>
-    <body>
-        <header>
-            <a href="./addLogs.php"> Add Log</a>
-        </header>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/logStyles.css">
+    <title>Logs</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="./index.html" tabindex="0">Home</a></li>
+            <li><a href="#about" tabindex="0">About</a></li>
+            <li><a href="#contact" tabindex="0">Contact</a></li>
+        </ul>
+    </nav>
+    <header>
+        <a href="./addLogs.php">Add Log</a>
+    </header>
+    <main>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Event</th>
+                <th>Location</th>
+                <th>App</th>
+            </tr>
+            <?php
+            require './php/env/database.php';
+
+            $sql = "SELECT * FROM dashboard.logs";
+            $stmt = $pdo->query($sql);
+
+            while ($row = $stmt->fetch()) {
+                echo
+                    "<tr><td>" . $row['id'] . "</td>" .
+                    "<td>" . date("m/d/Y", strtotime($row['date'])) . "</td>" .
+                    "<td>" . htmlspecialchars($row['description']) . "</td>" .
+                    "<td>" . $row['location'] . "</td>" .
+                    "<td>" . $row['application'] . "</td></tr>";
+            }
+            ?>
+        </table>
+    </main>
+
+</body>
+
 </html>
-
-<?php
-require './php/env/database.php';
-
-$sql = "SELECT * FROM dashboard.logs";
-$stmt = $pdo->query($sql);
-
-while ($row = $stmt->fetch()) {
-    // Process each row
-    echo "Date: " . date("m/d/Y", strtotime($row['date'])) . 
-    
-    " Event: " . $row['description'] . "<br>";
-}
-?>
